98030854b2cfc6b4a75ab4b3d188e229602566fb
Konstantin Prokofiev <flashboyz@yandex.ru>
2021-08-19 00:05:20 +0300

    1.Storage Manager 2.Some action with cell

--- CoreDataDemo/TaskListViewController.swift
+++ CoreDataDemo/TaskListViewController.swift
@@ -8,13 +8,9 @@
 import UIKit
 import CoreData
 
-protocol TaskViewControllerDelegate {
-    func reloadData()
-}
-
 class TaskListViewController: UITableViewController {
 
-    private let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+    private let context = StorageManager.shared.persistentContainer.viewContext
     private let cellID = "cell"
     private var taskList: [Task] = []
     
@@ -56,9 +52,7 @@ class TaskListViewController: UITableViewController {
     }
     
     @objc private func addNewTask() {
-        let newTaskVC = TaskViewController()
-        newTaskVC.delegate = self
-        present(newTaskVC, animated: true)
+        showAlert(with: "New task", and: "What do you want to do?")
     }
     
     private func fetchData() {
@@ -70,12 +64,52 @@ class TaskListViewController: UITableViewController {
             print(error.localizedDescription)
         }
     }
+    
+    private func showAlert(with title: String, and massage: String) {
+        let alert = UIAlertController(title: title, message: massage, preferredStyle: .alert)
+        let saveAction = UIAlertAction(title: "Save", style: .default) { _ in
+            guard let task = alert.textFields?.first?.text, !task.isEmpty else { return }
+            self.save(task)
+        }
+        let cancelAction = UIAlertAction(title: "Cancel", style: .destructive)
+        alert.addAction(saveAction)
+        alert.addAction(cancelAction)
+        alert.addTextField { textField in
+            textField.placeholder = "New task"
+        }
+        present(alert, animated: true)
+    }
+    
+    private func save(_ taskName: String) {
+        guard let entityDescription = NSEntityDescription.entity(forEntityName: "Task", in: context) else {
+            return
+        }
+        guard let task = NSManagedObject(entity: entityDescription, insertInto: context) as? Task else { return }
+        task.name = taskName
+        taskList.append(task)
+        
+        let cellIndex = IndexPath(row: taskList.count - 1, section: 0)
+        tableView.insertRows(at: [cellIndex], with: .automatic)
+        
+        if context.hasChanges {
+            do {
+                try context.save()
+            } catch let error {
+                print(error.localizedDescription)
+            }
+        }
+    }
 }
 
+// MARK: - Extensions for TaskListViewController
+
+
 extension TaskListViewController {
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         taskList.count
     }
+    
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: cellID, for: indexPath)
@@ -85,11 +119,52 @@ extension TaskListViewController {
         cell.contentConfiguration = content
         return cell
     }
-}
-
-extension TaskListViewController: TaskViewControllerDelegate {
-    func reloadData() {
-        fetchData()
-        tableView.reloadData()
+    
+    
+    override func tableView(_ tableView: UITableView, trailingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
+        
+        let deleteAction = UIContextualAction(style: .destructive, title: "Delete") { _, _, _ in
+            
+            let fetchRequest: NSFetchRequest<Task> = Task.fetchRequest()
+            guard let tasks = try? self.context.fetch(fetchRequest) else {return }
+            self.taskList.remove(at: indexPath.row)
+            self.context.delete(tasks[indexPath.row])
+            self.tableView.reloadData()
+            
+            do {
+                try self.context.save()
+            } catch let error {
+                print(error)
+            }
+        }
+        return UISwipeActionsConfiguration(actions: [deleteAction])
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let alert = UIAlertController(title: "Editing task ",
+                                      message: "What do you want to change in task?",
+                                      preferredStyle: .alert)
+        alert.addTextField { textField in
+            textField.text = self.taskList[indexPath.row].name
+        }
+        let editingAction = UIAlertAction(title: "Done", style: .cancel) { _ in
+            let fetchRequest: NSFetchRequest<Task> = Task.fetchRequest()
+            
+            guard let tasks = try? self.context.fetch(fetchRequest) else { return }
+            tasks[indexPath.row].name = alert.textFields?.first?.text
+            
+            self.taskList.remove(at: indexPath.row)
+            self.taskList.insert(tasks[indexPath.row], at: indexPath.row)
+            
+            do {
+                try self.context.save()
+            } catch let error {
+                print(error.localizedDescription)
+            }
+            self.tableView.reloadData()
+        }
+        alert.addAction(editingAction)
+        present(alert, animated: true)
     }
 }
+
